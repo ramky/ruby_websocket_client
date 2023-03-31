c527d8782b8bbcb0cb92eed5d3597c643072207a
Ram Iyer <ramky.iyer@gmail.com>
2023-03-31 17:52:21 -0400

    initial commit for websocket client

---
+++ Rakefile
@@ -0,0 +1,11 @@
+load 'lib/socket.rb'
+
+namespace :client do
+  desc "create websocket connection until server terminates it"
+  task :websocket_request do
+
+    puts 'Creating websocket request ...'
+
+    Socket.run
+  end
+end
---
+++ lib/socket.rb
@@ -0,0 +1,24 @@
+require 'faye/websocket'
+require 'eventmachine'
+
+class Socket
+  def self.run
+    EM.run {
+      ws = Faye::WebSocket::Client.new('ws://localhost:3000/cable')
+
+      ws.on :open do |event|
+        p [:open]
+        ws.send('Hello, world!')
+      end
+
+      ws.on :message do |event|
+        p [:message, event.data]
+      end
+
+      ws.on :close do |event|
+        p [:close, event.code, event.reason]
+        ws = nil
+      end
+    }
+  end
+end
